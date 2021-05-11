3d7aac949419a0cdc49546023974e3897d5b82a7
Martin Peralez <martin.peralez@outlook.com>
2021-05-11 12:05:38 -0500

    Created AppDbContext Class and REAL Repository Class (EfPetRepository)

---
+++ 18WeekProject/MyPets/Models/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPets.Models
+{
+    public class AppDbContext : DbContext
+    {
+        //   F i e l d s   &   P r o p e r t i e s
+        public DbSet<Pet> Pets { get; set; }
+
+        //   C o n s t r u c t o r s
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        //   M e t h o d s
+    }
+}
---
+++ 18WeekProject/MyPets/Models/Pet/EfPetRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPets.Models.Pet
+{
+    public class EfPetRepository : IPetRepository
+    {
+        //   F i e l d s   &   P r o p e r t i e s
+
+        private AppDbContext _context;
+
+        //   C o n s t r u c t o r s
+
+        public EfPetRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //   M e t h o d s
+
+        public IQueryable<Pet> GetAllPets()
+        {
+            return _context.Pets;
+        }
+    }
+}
